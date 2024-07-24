648df6cfbe74cfccb5200f4433c3f3da16f56064
caiocroccia22 <caiocroccia22@gmail.com>
2024-07-23 21:01:26 -0300

    fix: Scrapy spider configuration and naming issues

--- bookscrapy/bookscrapy/spiders/bookspider.py
+++ bookscrapy/bookscrapy/spiders/bookspider.py
@@ -7,4 +7,21 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+         # Seleciona todos os artigos de livros na página
+        books = response.css('article.product_pod')
+        
+        for book in books:
+            # Extrai o título e o preço do livro
+            title = book.css('h3 a::attr(title)').get()
+            price = book.css('div.product_price p.price_color::text').get()
+
+            # Retorna os dados extraídos
+            yield {
+                'title': title,
+                'price': price
+            }
+
+        # Segue para a próxima página, se existir
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
